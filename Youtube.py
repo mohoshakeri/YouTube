9dcb39dd82b6625a9fc367185a4fa34a56bab935
Hossein <hossein82shakeri@gmail.com>
2022-08-23 20:54:01 +0430

    Clean Code

--- Youtube.py
+++ Youtube.py
@@ -1,9 +1,9 @@
 from pytube import Playlist
 
-def playListDownloader(url,path,quality=360):
-    playList = Playlist(url)
+def play_list_downloader(url,path,quality=360):
+    play_list = Playlist(url)
     counter = 0
-    for video in playList.videos:
+    for video in play_list.videos:
         video.streams.filter(res=f"{quality}p").first().download(path)
         counter += 1
     return counter
