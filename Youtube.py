67e3c74accb69cf77b03c636c5a15aa6c2ea5096
Hossein <hossein82shakeri@gmail.com>
2022-08-25 17:28:47 +0430

    add function description

--- Youtube.py
+++ Youtube.py
@@ -1,6 +1,17 @@
 from pytube import Playlist
 
-def play_list_downloader(url,path,quality=360):
+def play_list_downloader(url:str,path:str,quality:int=480):
+    """
+    Download all videos of a youtube playlist
+    
+    params:
+        url (str) : youtube link\n
+        path (str) : The directory that downloaded videos will be saved there\n
+        quality (int) [optional] : quality of videos (defult = 480)
+        
+    Return:
+        Number of downloaded videos
+    """
     play_list = Playlist(url)
     counter = 0
     for video in play_list.videos:
