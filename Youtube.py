814648da5a57a6fd737ae44183f7d408cb38beb1
Hossein <hossein82shakeri@gmail.com>
2022-08-27 18:24:53 +0430

    Correct spelling mistakes

--- Youtube.py
+++ Youtube.py
@@ -1,14 +1,15 @@
 from pytube import Playlist
 
-def play_list_downloader(url:str,path:str,quality:int=480):
+
+def play_list_downloader(url: str, path: str, quality: int = 480):
     """
     Download all videos of a youtube playlist
-    
+
     params:
         url (str) : youtube link\n
         path (str) : The directory that downloaded videos will be saved there\n
-        quality (int) [optional] : quality of videos (defult = 480)
-        
+        quality (int) [optional] : quality of videos (default = 480)
+
     Return:
         Number of downloaded videos
     """
@@ -18,4 +19,3 @@ def play_list_downloader(url:str,path:str,quality:int=480):
         video.streams.filter(res=f"{quality}p").first().download(path)
         counter += 1
     return counter
-
